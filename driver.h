7b2590bdbfcd68cc4ef4deebb7e39cb3c5cebc4f
coord.e <me@coord-e.com>
2018-10-11 21:38:33 +0900

    Add: Add a convenient function to read and write byte

--- driver.h
+++ driver.h
@@ -29,6 +29,20 @@ public:
   }
 
 private:
+  void write_byte(std::uint8_t data) {
+    if(this->write(&data, 1) != 1) {
+      throw std::runtime_error("Failed to write byte");
+    }
+  }
+
+  std::uint8_t read_byte() {
+    std::uint8_t data;
+    if(this->read(&data, 1) != 1) {
+      throw std::runtime_error("Failed to read byte");
+    }
+    return data;
+  }
+
   std::size_t write(const void* buf, std::size_t count) {
     return ::write(this->fd, buf, count);
   }
