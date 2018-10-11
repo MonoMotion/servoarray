7ee466d5047b2f3ad201c1b122fda537cf837579
coord.e <me@coord-e.com>
2018-10-11 22:23:17 +0900

    Change: Separate file

--- pca9685.h
+++ pca9685.h
@@ -11,29 +11,7 @@
 #include <stdexcept>
 #include <type_traits>
 
-enum class Register : std::uint8_t {
-  MODE1 = 0x00,
-  MODE2 = 0x01,
-  SUBADR1 = 0x02,
-  SUBADR2 = 0x03,
-  SUBADR3 = 0x04,
-  ALLCALLADR = 0x05,
-  LED0 = 0x6,
-  LED0_ON_L = 0x6,
-  LED0_ON_H = 0x7,
-  LED0_OFF_L = 0x8,
-  LED0_OFF_H = 0x9,
-  ALLLED_ON_L = 0xFA,
-  ALLLED_ON_H = 0xFB,
-  ALLLED_OFF_L = 0xFC,
-  ALLLED_OFF_H = 0xFD,
-  PRE_SCALE = 0xFE
-};
-
-Register operator+(const Register& reg, const std::uint8_t& offset) {
-  const auto address = static_cast<uint8_t>(reg) + offset;
-  return Register(address);
-}
+#include "register.h"
 
 static constexpr float CLOCK_FREQ = 25000000.0;
 
---
+++ register.h
@@ -0,0 +1,28 @@
+#ifndef SERVOARRAY_REGISTER_H
+#define SERVOARRAY_REGISTER_H
+
+enum class Register : std::uint8_t {
+  MODE1 = 0x00,
+  MODE2 = 0x01,
+  SUBADR1 = 0x02,
+  SUBADR2 = 0x03,
+  SUBADR3 = 0x04,
+  ALLCALLADR = 0x05,
+  LED0 = 0x6,
+  LED0_ON_L = 0x6,
+  LED0_ON_H = 0x7,
+  LED0_OFF_L = 0x8,
+  LED0_OFF_H = 0x9,
+  ALLLED_ON_L = 0xFA,
+  ALLLED_ON_H = 0xFB,
+  ALLLED_OFF_L = 0xFC,
+  ALLLED_OFF_H = 0xFD,
+  PRE_SCALE = 0xFE
+};
+
+Register operator+(const Register& reg, const std::uint8_t& offset) {
+  const auto address = static_cast<uint8_t>(reg) + offset;
+  return Register(address);
+}
+
+#endif
