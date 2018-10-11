e007815a19881a0488660e04e9a36c788477c0e7
coord.e <me@coord-e.com>
2018-10-11 22:50:46 +0900

    Change: Enable to specify index from argv

--- src/set_pwm.cpp
+++ src/set_pwm.cpp
@@ -4,13 +4,14 @@
 
 int main(int argc, char **argv) {
 
-  if (argc != 3) {
-    fprintf(stderr, "set_pwm <bus> <address>\n");
+  if (argc != 4) {
+    std::cerr << "set_pwm <bus> <address> <index>" << std::endl;
     return 1;
   }
 
   const std::uint8_t bus = strtol(argv[1], NULL, 0);
-  const std::uint8_t addr = strtol(argv[2], NULL, 0);
+  const std::uint8_t addr = strtol(argv[2], NULL, 8);
+  const std::uint8_t index = strtol(argv[3], NULL, 8);
 
   auto driver = PCA9685(bus, addr);
 
@@ -18,7 +19,7 @@ int main(int argc, char **argv) {
     std::uint16_t pulselen;
     std::cout << "> ";
     std::cin >> pulselen;
-    driver.set_pwm(0, 0, pulselen);
+    driver.set_pwm(index, 0, pulselen);
   }
   return 0;
 }
