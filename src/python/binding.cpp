dfcf5b4a56d085010905f51bcd0d88f0bb3a6e69
coord.e <me@coord-e.com>
2018-10-12 21:18:56 +0900

    Add: Implement slice indexing in __getitem__

--- src/python/binding.cpp
+++ src/python/binding.cpp
@@ -14,6 +14,7 @@
 // along with servoarray.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <utility>
+#include <cstdlib>
 
 #include <pybind11/pybind11.h>
 
@@ -32,6 +33,18 @@ public:
 
   void set(std::uint8_t index, double rad) { this->sa.set(index, rad); }
   double get(std::uint8_t index) { return this->sa.get(index); }
+  py::list get(py::slice slice) {
+    std::size_t start, stop, step, length;
+    if (!slice.compute(this->sa.size(), &start, &stop, &step, &length))
+      throw py::error_already_set();
+
+    auto l = py::list();
+    for (std::size_t i = 0; i < length; ++i) {
+      l.append(this->sa.get(start));
+      start += step;
+    }
+    return l;
+  }
 
   std::uint8_t size() { return this->sa.size(); }
 };
@@ -43,9 +56,10 @@ PYBIND11_MODULE(servoarray, m) {
   py::class_<Adaptor::ServoArray>(m, "ServoArray")
     .def(py::init<std::uint8_t, std::uint8_t, std::uint16_t, std::uint16_t>(), py::arg("bus") = 1, py::arg("address") = 0x40, py::arg("min_pulse") = 150, py::arg("max_pulse") = 600)
     .def("set", &Adaptor::ServoArray::set)
-    .def("get", &Adaptor::ServoArray::get)
+    .def("get", py::overload_cast<std::uint8_t>(&Adaptor::ServoArray::get))
     .def("__len__", &Adaptor::ServoArray::size)
     .def("__setitem__", &Adaptor::ServoArray::set)
-    .def("__getitem__", &Adaptor::ServoArray::get);
+    .def("__getitem__", py::overload_cast<py::slice>(&Adaptor::ServoArray::get))
+    .def("__getitem__", py::overload_cast<std::uint8_t>(&Adaptor::ServoArray::get));
 }
 
