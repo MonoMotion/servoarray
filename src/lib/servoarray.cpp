8419fc48d9ac7b5caeaac98fdbd7bf2fb61658ec
coord.e <me@coord-e.com>
2018-10-12 21:54:42 +0900

    Add: Add a bound check before setting position value

--- src/lib/servoarray.cpp
+++ src/lib/servoarray.cpp
@@ -25,6 +25,10 @@ ServoArray::ServoArray(std::uint8_t bus, std::uint8_t address, std::uint16_t min
 }
 
 void ServoArray::set(std::uint8_t index, double rad) {
+  if (rad > M_PI / 2 || rad < - M_PI / 2) {
+    throw std::out_of_range("Position value must be within the range from -pi/2 to pi/2");
+  }
+
   this->values[index] = rad;
 
   const auto pulse = (rad + M_PI / 2) * (this->max_pulse - this->min_pulse) / M_PI + this->min_pulse;
