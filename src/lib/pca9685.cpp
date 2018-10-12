367d872bc2d7da6b5dfbac29605e797688a6cf8d
coord.e <me@coord-e.com>
2018-10-12 20:45:16 +0900

    Fix: Fix typo

--- src/lib/pca9685.cpp
+++ src/lib/pca9685.cpp
@@ -21,7 +21,7 @@ PCA9685::PCA9685(std::uint8_t bus, std::uint8_t addr) : bus(bus), address(addr) 
   char dev_path[64];
   std::sprintf(dev_path, "/dev/i2c-%d", this->bus);
   if ((this->fd = ::open(dev_path, O_RDWR)) < 0) {
-    throw std::runtime_error("Faild to open i2c device file " + std::string{ dev_path });
+    throw std::runtime_error("Failed to open i2c device file " + std::string{ dev_path });
   }
 
   if (ioctl(this->fd, I2C_SLAVE, this->address) < 0) {
