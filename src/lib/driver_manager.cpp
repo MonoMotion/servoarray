efd4da62a935258d78393418a614f1f158ddce7e
coord.e <me@coord-e.com>
2019-01-20 12:39:10 +0000

    Add: Implement load / get_or_load

--- src/lib/driver_manager.cpp
+++ src/lib/driver_manager.cpp
@@ -1,5 +1,7 @@
 #include "servoarray/driver_manager.h"
 
+#include <dlfcn.h>
+
 namespace ServoArray {
 
 DriverManager::DriverManager(const std::string& path) : path_(path), loaded_drivers_() {}
@@ -7,6 +9,27 @@ DriverManager::DriverManager(const std::string& path) : path_(path), loaded_driv
 Driver* DriverManager::get(const std::string& name) const {
   // TODO: throw errors::DriverNotFoundError when the name is not found
   return this->loaded_drivers_.at(name);
+}
+
+Driver* DriverManager::load(const std::string& name) {
+  const auto path = this->resolve(name);
+  auto* handle = dlopen(path.c_str(), RTLD_LAZY);
+  if (!handle) {
+    // TODO: throw errors::CouldNotLoadDriverError
+    // TODO: Use dlerror(3) to obtain error message
+    throw std::runtime_error("Could not load " + path);
+  }
+
+  auto* f = static_cast<Driver* (*)()>(dlsym(handle, "servoarray_driver"));
+  return f();
+}
+
+Driver* DriverManager::get_or_load(const std::string& name) {
+  if (this->is_loaded(name)) {
+    return this->get(name);
+  } else {
+    return this->load(name);
+  }
 }
 
 bool DriverManager::is_loaded(const std::string& name) const {
