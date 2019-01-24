cea1ff0265c34c46c53352551f7460dfb0857e84
coord.e <me@coord-e.com>
2019-01-24 15:11:11 +0000

    Add: Add dummy driver

---
+++ src/config/tools.cmake
@@ -0,0 +1,36 @@
+# This file is part of servoarray.
+#
+# servoarray is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# servoarray is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with servoarray.  If not, see <http://www.gnu.org/licenses/>.
+
+function(servoarray_add_driver target)
+  cmake_parse_arguments(ARG "" "NAME" "" ${ARGN})
+
+  if(ARG_NAME)
+    set(name ${ARG_NAME})
+  else()
+    set(name ${target})
+  endif()
+
+  add_library(${target} SHARED ${ARG_UNPARSED_ARGUMENTS})
+  set_target_properties(${target} PROPERTIES PREFIX "")
+  set_target_properties(${target} PROPERTIES OUTPUT_NAME "${name}")
+  set_target_properties(${target} PROPERTIES POSITION_INDEPENDENT_CODE TRUE)
+  set_target_properties(${target} PROPERTIES MACOSX_RPATH ON)
+
+  execute_process(COMMAND ${CMAKE_CXX_COMPILER} -dumpmachine OUTPUT_VARIABLE host_triple OUTPUT_STRIP_TRAILING_WHITESPACE)
+  set_target_properties(${target} PROPERTIES SUFFIX ".servoarray-${servoarray_VERSION_MAJOR}-${host_triple}.so")
+
+  target_link_libraries(${target} ${SERVOARRAY_DRIVER_LIBRARIES})
+  target_include_directories(${target} ${inc_system} PRIVATE ${SERVOARRAY_INCLUDE_DIR})
+endfunction()
---
+++ src/dummy-driver/dummy.cpp
@@ -0,0 +1,30 @@
+#include <servoarray/driver.h>
+#include <cstdint>
+#include <iostream>
+
+class DummyDriver final : public ServoArray::Driver {
+  std::size_t size_;
+
+public:
+  DummyDriver(std::size_t size) : size_(size) {}
+
+  void write(std::size_t idx, double pos) override {
+    std::cout << "[write] index: " << idx << "\t position: " << pos << std::endl;
+  }
+
+  double read(std::size_t idx) const override {
+    std::cout << "[read]  index: " << idx << std::endl;
+    return 0;
+  }
+
+  std::size_t size() const override {
+    return this->size_;
+  }
+};
+
+extern "C" ServoArray::Driver* servoarray_driver(const ServoArray::DriverParams& params) {
+  for(auto const& p : params) {
+    std::cout << "[init]  key: " << p.first << "\t value: " << p.second.data() << std::endl;
+  }
+  return new DummyDriver(params.get_or<std::size_t>("size", 5));
+}
