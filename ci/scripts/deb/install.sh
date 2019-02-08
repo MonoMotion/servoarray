6774d7377c33adca12422f2927f668955c148527
coord.e <me@coord-e.com>
2019-02-08 03:24:01 +0000

    [WIP] Add: Add install scripts

---
+++ ci/scripts/deb/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# This file is part of servoarray.
+#
+# servoarray is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# servoarray is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with servoarray.  If not, see <http://www.gnu.org/licenses/>.
+
+set -euo pipefail
+
+function base_image() {
+  if [ -n "${CROSS:-}" ]; then
+    echo "balenalib/${CROSS}:${DEBIAN_VERSION}-build"
+  else
+    echo "debian:${DEBIAN_VERSION}-slim"
+  fi
+}
+
+docker build ci/image -t builder --build-arg BASE_IMAGE=$(base_image)
