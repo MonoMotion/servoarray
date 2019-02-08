1eda18bb8c97227d5db17d78c70e436ae1d3b1ed
coord.e <me@coord-e.com>
2019-02-08 02:22:48 +0000

    Change: Calculate suffix in servoarrayTools.cmake

--- src/config/servoarrayTools.cmake
+++ src/config/servoarrayTools.cmake
@@ -13,6 +13,10 @@
 # You should have received a copy of the GNU General Public License
 # along with servoarray.  If not, see <http://www.gnu.org/licenses/>.
 
+execute_process(COMMAND uname -m OUTPUT_VARIABLE SERVOARRAY_TARGET_MACHINE OUTPUT_STRIP_TRAILING_WHITESPACE)
+execute_process(COMMAND uname -s OUTPUT_VARIABLE SERVOARRAY_TARGET_ABI OUTPUT_STRIP_TRAILING_WHITESPACE)
+set(SERVOARRAY_DRIVER_SUFFIX ".servoarray-${servoarray_VERSION_MAJOR}-${SERVOARRAY_TARGET_MACHINE}-${SERVOARRAY_TARGET_ABI}.so")
+
 function(servoarray_add_driver target)
   cmake_parse_arguments(ARG "" "NAME" "" ${ARGN})
 
@@ -27,10 +31,7 @@ function(servoarray_add_driver target)
   set_target_properties(${target} PROPERTIES OUTPUT_NAME "${name}")
   set_target_properties(${target} PROPERTIES POSITION_INDEPENDENT_CODE TRUE)
   set_target_properties(${target} PROPERTIES MACOSX_RPATH ON)
-
-  execute_process(COMMAND uname -m OUTPUT_VARIABLE machine OUTPUT_STRIP_TRAILING_WHITESPACE)
-  execute_process(COMMAND uname -s OUTPUT_VARIABLE abi OUTPUT_STRIP_TRAILING_WHITESPACE)
-  set_target_properties(${target} PROPERTIES SUFFIX ".servoarray-${servoarray_VERSION_MAJOR}-${machine}-${abi}.so")
+  set_target_properties(${target} PROPERTIES SUFFIX ${SERVOARRAY_DRIVER_SUFFIX})
 
   target_link_libraries(${target} ${SERVOARRAY_DRIVER_LIBRARIES})
   target_include_directories(${target} ${inc_system} PRIVATE ${SERVOARRAY_INCLUDE_DIR})
