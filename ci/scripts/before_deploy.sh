cd9002251167bd01901cec68d8045ba2c19291b1
coord.e <me@coord-e.com>
2019-02-09 15:01:16 +0000

    Add: Add bintray deploy configuration

---
+++ ci/scripts/before_deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# This file is part of servoarray.
+#
+# servoarray is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# servoarray is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with servoarray.  If not, see <http://www.gnu.org/licenses/>.
+
+set -euo pipefail
+
+DEB_ARCH=$(docker run builder "dpkg --print-architecture")
+
+cat ci/bintray.in \
+  | sed "s/@VERSION@/$(./version.sh)/g" \
+  | sed "s/@DEBIAN_VERSION@/${DEBIAN_VERSION}/g" \
+  | sed "s/@ARCH@/${DEB_ARCH}/g" \
+  > bintray.json
