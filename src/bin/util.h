b83d3af26ab277b38a11ed27ccd738cd97ffd430
coord.e <me@coord-e.com>
2019-02-11 12:26:52 +0000

    Change: Separete signal handling process

--- src/bin/util.h
+++ src/bin/util.h
@@ -13,32 +13,35 @@
 // You should have received a copy of the GNU General Public License
 // along with servoarray.  If not, see <http://www.gnu.org/licenses/>.
 
-#include "servoarray/servoarray.h"
-#include "./util.h"
+#ifndef SERVOARRAY_BIN_UTILS_H
+#define SERVOARRAY_BIN_UTILS_H
 
-#include <iostream>
+#include <atomic>
+#include <signal.h>
 
-int main(int argc, char **argv) {
+namespace util {
 
-  const std::string name {argc > 1 ? argv[1] : ""};
-  auto sa = ServoArray::ServoArray(name);
+static std::atomic<bool> quit(false);
 
-  util::register_signal(SIGINT);
-  util::register_signal(SIGQUIT);
-  util::register_signal(SIGTERM);
+void register_signal(int);
+void quit_handler(int);
+bool should_exit();
 
-  while(true) {
-    std::size_t index;
-    std::cout << "(index) > ";
-    std::cin >> index;
-
-    double rad;
-    std::cout << " (rad)  > ";
-    std::cin >> rad;
-
-    sa[index] = rad;
-    std::cout << index << " -> " << sa[index] << std::endl;
-
-    if(util::should_exit()) break;
-  }
+void register_signal(int signal) {
+  struct sigaction action{};
+  action.sa_handler = quit_handler;
+  sigfillset(&action.sa_mask);
+  sigaction(signal, &action, nullptr);
 }
+
+void quit_handler(int) {
+  quit.store(true);
+}
+
+bool should_exit() {
+  return quit.load();
+}
+
+}
+
+#endif
